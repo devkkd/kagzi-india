bf488c591e24f7097fc46703385ef624ed695300
roxhannn <roshanludhani590@gmail.com>
2026-05-08 19:00:21 +0530

    Kagzi-India

---
+++ src/app/sitemap.js
@@ -0,0 +1,54 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.kagziindia.com/",
+      changeFrequency: "daily",
+      priority: 1.0,
+    },
+    {
+      url: "https://www.kagziindia.com/about/",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.kagziindia.com/our-legacy/",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.kagziindia.com/papermaking/",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.kagziindia.com/collections/",
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.kagziindia.com/products/",
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.kagziindia.com/products?category=69d77484aa4642cb6232dbe3&subcategory=69d774e4aa4642cb6232dbed",
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+    {
+      url: "https://www.kagziindia.com/faqs/",
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://www.kagziindia.com/contact/",
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://www.kagziindia.com/request-quote/",
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
+}
